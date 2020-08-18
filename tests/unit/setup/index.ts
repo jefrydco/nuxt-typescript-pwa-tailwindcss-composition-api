20a95e0bb68c52a6fa0d5e65c0fffb1351c9ee94
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 12:56:08 +0700

    feat(unit): stubs nuxt no ssr and nuxt link component

--- tests/unit/setup/index.ts
+++ tests/unit/setup/index.ts
@@ -1,8 +1,14 @@
-import { config } from '@vue/test-utils'
+import { config, RouterLinkStub } from '@vue/test-utils'
 import { Context as NuxtContext } from '@nuxt/types'
 
 const mockNuxtContext: Partial<NuxtContext> = {
   redirect: jest.fn()
+}
+
+config.stubs = {
+  nuxt: true,
+  'no-ssr': true,
+  'nuxt-link': RouterLinkStub
 }
 
 config.mocks = {
