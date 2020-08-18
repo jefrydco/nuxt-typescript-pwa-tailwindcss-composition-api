456dcd0f88ebf6094f46bc2f027a3ecf9fcce9ef
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 19:32:51 +0700

    test(e2e): add some assertion

--- tests/e2e/integration/index.spec.ts
+++ tests/e2e/integration/index.spec.ts
@@ -1,6 +1,12 @@
 describe('Example Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
+    cy.contains('.btn__set', 'Set User Store')
+    cy.screenshot()
+  })
+  it('Visits the random image url', () => {
+    cy.get('a[href="/random-image"]').click()
+    cy.get('.img__random').should('have.attr', 'alt', 'Random Image')
     cy.screenshot()
   })
 })
