f0d460660c246ef36243c3ed0dc4813f61ccbf2f
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 13:04:22 +0700

    test(unit-layouts-error): add snapshot test

---
+++ tests/unit/specs/layouts/error.spec.ts
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import ErrorLayout from '~/layouts/error.vue'
+
+describe('error layout', () => {
+  it('renders to snapshot', () => {
+    expect.assertions(1)
+    const wrapper = shallowMount(ErrorLayout)
+    expect(wrapper).toMatchInlineSnapshot(``)
+  })
+})
