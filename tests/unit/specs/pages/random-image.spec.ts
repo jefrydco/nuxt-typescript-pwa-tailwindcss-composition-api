73d90f227c6c37917d52cd82b73eb036add31493
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 19:54:09 +0700

    test(unit-pages-random-image): add snapshot

---
+++ tests/unit/specs/pages/random-image.spec.ts
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import RandomImagePage from '~/pages/random-image.vue'
+
+describe('random image page', () => {
+  it('renders to snapshot', () => {
+    expect.assertions(1)
+    const wrapper = shallowMount(RandomImagePage)
+    wrapper.setData({ imageUrl: 'https://source.unsplash.com/random/500x400' })
+    expect(wrapper).toMatchInlineSnapshot(
+      `<div><img src="https://source.unsplash.com/random/500x400" alt="Random Image" width="500" height="400" class="img__random rounded shadow-lg"></div>`
+    )
+  })
+})
