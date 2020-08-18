ef677b816c16b575f72ac468609854a7f190230f
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 13:04:13 +0700

    test(unit-layouts-default): add snapshot test

---
+++ tests/unit/specs/layouts/default.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import DefaultLayout from '~/layouts/default.vue'
+
+describe('default layout', () => {
+  it('renders to snapshot', () => {
+    expect.assertions(1)
+    const wrapper = shallowMount(DefaultLayout)
+    expect(wrapper).toMatchInlineSnapshot(`
+      <main class="container mx-auto px-4 lg:px-8 pt-4">
+        <app-appear-disappear-stub></app-appear-disappear-stub>
+        <app-links-stub items="[object Object],[object Object]"></app-links-stub>
+        <div class="flex flex-wrap relative">
+          <nuxt class="w-full lg:w-4/5"></nuxt>
+        </div>
+        <app-switch-theme-stub></app-switch-theme-stub>
+      </main>
+    `)
+  })
+})
