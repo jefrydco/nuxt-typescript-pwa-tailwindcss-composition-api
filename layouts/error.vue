373597861d71902f6066efee9438874fb823ee68
Jefry Dewangga <jefrydco@gmail.com>
2020-08-18 14:08:14 +0700

    refactor(layouts-error): change to use render function instead of returning from setup

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,12 @@
 <script lang="ts">
-import { h, defineComponent, useContext } from '@nuxtjs/composition-api'
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
     const { redirect } = useContext()
     redirect('/')
-
+  },
+  render(h) {
     return h('div')
   }
 })
